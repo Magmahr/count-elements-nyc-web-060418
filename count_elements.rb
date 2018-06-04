62c24702db351c486a319a002579a6f1db9b20d0
Matt Maher <mattmaher0@gmail.com>
2018-06-04 21:43:31 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,8 +1,8 @@
 def count_elements(array)
   new_hash = {}
-  count = 1
+  count = 0
   array.each do |animal|
-    new_hash[animal] = count
+    new_hash[animal] = count + 1
       if animal
         count += 1
       end 
